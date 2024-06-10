c035245608fe7f1cc7d12927f07830397c1328ca
Eagle <110081590+eaglehuntt@users.noreply.github.com>
2024-06-10 23:18:34 +0000

    New build system that now logs errors

--- utils/build.js
+++ utils/build.js
@@ -3,25 +3,45 @@ process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
 
-var webpack = require('webpack'),
-  path = require('path'),
-  fs = require('fs'),
-  config = require('../webpack.config'),
-  ZipPlugin = require('zip-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const config = require('../webpack.config');
+const ZipPlugin = require('zip-webpack-plugin');
 
-delete config.chromeExtensionBoilerplate;
+async function build() {
+  try {
+    delete config.chromeExtensionBoilerplate;
 
-config.mode = 'production';
+    config.mode = 'production';
 
-var packageInfo = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+    const packageInfo = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 
-config.plugins = (config.plugins || []).concat(
-  new ZipPlugin({
-    filename: `${packageInfo.name}-${packageInfo.version}.zip`,
-    path: path.join(__dirname, '../', 'zip'),
-  })
-);
+    config.plugins = (config.plugins || []).concat(
+      new ZipPlugin({
+        filename: `${packageInfo.name}-${packageInfo.version}.zip`,
+        path: path.join(__dirname, '../', 'zip'),
+      })
+    );
 
-webpack(config, function (err) {
-  if (err) throw err;
-});
+    await new Promise((resolve, reject) => {
+      webpack(config, (err, stats) => {
+        if (err) {
+          console.error('Webpack build error:', err);
+          reject(err);
+        } else if (stats.hasErrors()) {
+          const info = stats.toJson();
+          console.error('Webpack build errors:', info.errors);
+          reject(new Error('Webpack build failed with errors.'));
+        } else {
+          console.log('Webpack build completed successfully.');
+          resolve();
+        }
+      });
+    });
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+build();
